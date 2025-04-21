41a049f6f3b56d2f34f15471c0ce4a037d8c18e9
Aldi-H <aldihimawan88@gmail.com>
2025-04-21 13:08:31 +0700

    chore: enhance ESLint configuration with unicorn plugin and custom rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,49 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import eslintPluginUnicorn from "eslint-plugin-unicorn";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+	baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+	...compat.config({
+		plugins: [eslintPluginUnicorn],
+		extends: ["next/core-web-vitals", "next/typescript"],
+		rules: [
+			{
+        "no-console": "error",
+        "no-unused-var" : "error",
+        "no-var" : "error",
+        "@typescript-eslint/no-explicit-any": "error",
+        "no-alert": "error",
+        "consistent-return": "error",
+        "no-nested-ternary" : "off"
+			},
+			eslintPluginUnicorn.configs.recommended,
+			{
+				rules: {
+					"unicorn/filename-case": [
+						"error",
+						{
+							case: "kebabCase",
+							ignore: [
+								"package.json",
+								"package-lock.json",
+								"README.md",
+								"LICENSE",
+								"CHANGELOG.md",
+							],
+						},
+          ],
+				},
+			},
+		],
+	}),
 ];
 
 export default eslintConfig;
