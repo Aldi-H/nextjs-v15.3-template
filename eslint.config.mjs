b870cbcf12f8dbff27b6eab965bef57645a5e7ce
Aldi-H <aldihimawan88@gmail.com>
2025-04-23 08:50:23 +0700

    chore: update import statements to use node module paths in ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 import { FlatCompat } from "@eslint/eslintrc";
 import eslintPluginUnicorn from "eslint-plugin-unicorn";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+import dirname from "node:path";
+import fileURLToPath from "node:url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
